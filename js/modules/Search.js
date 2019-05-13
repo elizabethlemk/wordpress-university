40a4361b075a0d9cfef82c76fb3dbbe8617f8207
Elizabeth Le <elizabethlemk@yahoo.com>
2019-05-13 15:11:13 -0400

    search

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -3,6 +3,7 @@ import $ from "jquery";
 class Search {
   // 1. describe and create/initiate object
   constructor() {
+    this.overlay = false;
     this.openBtn = $(".js-search-trigger");
     this.closeBtn = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
@@ -13,17 +14,30 @@ class Search {
   events() {
     this.openBtn.on("click", this.openOverlay.bind(this));
     this.closeBtn.on("click", this.closeOverlay.bind(this));
+    $(document).on("keyup", this.keyPress.bind(this));
   }
 
   // 3. methods
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    console.log("open");
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
+    console.log("close");
+  }
+
+  keyPress(event) {
+    if (event.keyCode == 83 && !this.overlay) {
+      this.openOverlay();
+      this.overlay = true;
+    } else if (event.keyCode == 27 && this.overlay) {
+      this.closeOverlay();
+      this.overlay = false;
+    }
   }
 }
 
