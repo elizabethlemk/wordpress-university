da4b94d970e159c97b9fae273e16bac8a8808c72
Elizabeth Le <elizabethlemk@yahoo.com>
2019-05-14 10:54:04 -0400

    endpoints

---
+++ includes/search-route.php
@@ -0,0 +1,27 @@
+<?php
+  add_action('rest_api_init', 'uniRegisterSearch');
+
+  function uniRegisterSearch (){
+    register_rest_route('university/v1', 'search', array(
+      'methods' => WP_REST_SERVER::READABLE,
+      'callback' => 'uniSearchResults'
+    ));
+  }
+
+  function uniSearchResults($data){
+    $professors = new WP_Query(array(
+      'post_type' => 'professor',
+      's' => sanitize_text_field($data['term'])
+    ));
+
+    $results = array();
+    while ($professors->have_posts()) {
+      $professors->the_post();
+      array_push($results, array(
+        'title' => get_the_title(),
+        'url' => get_the_permalink()
+      ));
+    }
+    return $results;
+  }
+ ?>
