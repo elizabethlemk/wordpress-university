cf95b5dee7a263b4fd0c01b44ecf86184f1c5449
Elizabeth Le <elizabethlemk@yahoo.com>
2019-05-14 11:38:11 -0400

    endpoints

--- includes/search-route.php
+++ includes/search-route.php
@@ -9,19 +9,48 @@
   }
 
   function uniSearchResults($data){
-    $professors = new WP_Query(array(
-      'post_type' => 'professor',
+    $mainQuery = new WP_Query(array(
+      'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
       's' => sanitize_text_field($data['term'])
     ));
 
-    $results = array();
-    while ($professors->have_posts()) {
-      $professors->the_post();
-      array_push($results, array(
-        'title' => get_the_title(),
-        'url' => get_the_permalink()
-      ));
+    $results = array(
+      'general' => array(),
+      'professors' => array(),
+      'programs' => array(),
+      'events' => array(),
+      'campuses' => array()
+    );
+    while ($mainQuery->have_posts()) {
+      $mainQuery->the_post();
+      if (get_post_type() == 'post' or get_post_type() == 'page') {
+        array_push($results['general'], array(
+          'title' => get_the_title(),
+          'url' => get_the_permalink()
+        ));
+      } elseif (get_post_type() == 'professor') {
+        array_push($results['professors'], array(
+          'title' => get_the_title(),
+          'url' => get_the_permalink()
+        ));
+      } elseif (get_post_type() == 'program') {
+        array_push($results['programs'], array(
+          'title' => get_the_title(),
+          'url' => get_the_permalink()
+        ));
+      } elseif (get_post_type() == 'campus') {
+        array_push($results['campuses'], array(
+          'title' => get_the_title(),
+          'url' => get_the_permalink()
+        ));
+      } elseif (get_post_type() == 'event') {
+        array_push($results['events'], array(
+          'title' => get_the_title(),
+          'url' => get_the_permalink()
+        ));
+      }
     }
+
     return $results;
   }
  ?>
